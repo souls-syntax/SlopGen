0555729b2e6cf5fe59f0980bc694d224aa4e1340
Aakarsh Kashyap <souls.syntax@gmail.com>
2026-04-14 04:07:21 +0530

    Agent workflow working with read, write and execute functionality

--- app/main.go
+++ app/main.go
@@ -6,7 +6,10 @@ import (
 	"flag"
 	"fmt"
 	"os"
-
+	"strings"
+	"bufio"
+	"os/exec"
+	"runtime"
 	"github.com/openai/openai-go/v3"
 	"github.com/openai/openai-go/v3/option"
 	"github.com/souls-syntax/SlopGen/app/internal/model"
@@ -19,6 +22,8 @@ func main() {
 	// flag.StringVar(&NGROK_LINK, )
 	var prompt string
 	flag.StringVar(&prompt, "p", "", "Prompt to send to LLM")
+	var ngrok_url string
+	flag.StringVar(&ngrok_url, "n", "", "Link of the superior intelligence")
 	// So we are taking the prompt here. Likely
 
 	flag.Parse()
@@ -28,9 +33,13 @@ func main() {
 	}
 
 	// apiKey := os.Getenv("OPENROUTER_API_KEY")
-	baseUrl := os.Getenv("OPENROUTER_BASE_URL")
+	var model string
+	baseUrl := ngrok_url + "/v1"
 	if baseUrl == "" {
 		baseUrl = "http://localhost:11434/v1"
+		model = "qwen2.5:7b"
+	} else {
+		model = "gpt-oss:20b"
 	}
 	readTool := model.Tool{
 		Type: "function",
@@ -49,10 +58,63 @@ func main() {
 			},
 		},
 	}
-	res, _ := readTool.ConvertToOpenAITool()
 
+	writeTool := model.Tool{
+		Type: "function",
+		Function: model.Function{
+			Name:        "Write",
+			Description: "Write content to a file. Creates the file if it doesn't exist",
+			Parameters: model.Parameters{
+				Type: "object",
+				Properties: map[string]model.Property{
+					"file_path": {
+						Type:        "string",
+						Description: "The path to the file to write",
+					},
+					"content" : {
+						Type:        "string",
+						Description: "Content to write to the file",
+					},
+				},
+				Required: []string{"file_path", "content"},
+			},
+		},
+	}
+
+	executeTool := model.Tool{
+		Type: "function",
+		Function: model.Function{
+			Name:        "Execute",
+			Description: "Execute a shell command and return it's output (stdout + stderr)",
+			Parameters: model.Parameters{
+				Type: "object",
+				Properties: map[string]model.Property{
+					"command": {
+						Type:        "string",
+						Description: "The shell command to execute",
+					},
+				},
+				Required: []string{"file_path", "content"},
+			},
+		},
+	}
+	res, _ := readTool.ConvertToOpenAITool()
+	writ, _ := writeTool.ConvertToOpenAITool()
+	exect, _ := executeTool.ConvertToOpenAITool()
+	cwd, _ := os.Getwd()
+	systemPrompt := fmt.Sprintf(
+    "You are a helpful assistant. The current working directory is: %s. When asked to read a file, use the Read tool with paths relative to this directory.",
+    cwd,
+	)
 	client := openai.NewClient(option.WithAPIKey("ollama"), option.WithBaseURL(baseUrl))
 	messages := []openai.ChatCompletionMessageParamUnion{
+				{
+					OfSystem: &openai.ChatCompletionSystemMessageParam{
+    				Content: openai.ChatCompletionSystemMessageParamContentUnion{
+        			OfString: openai.String(systemPrompt),
+    				},
+					},
+				},
 				{
 					OfUser: &openai.ChatCompletionUserMessageParam{
 						Content: openai.ChatCompletionUserMessageParamContentUnion{
@@ -60,14 +122,15 @@ func main() {
 							// How to feel array to openAI go api?
 							OfString: openai.String(prompt),
 						},
-					},
+					},	
 				},
 			}
-		tools :=  []openai.ChatCompletionToolUnionParam{res}
+		tools :=  []openai.ChatCompletionToolUnionParam{res, writ, exect}
+	reader := bufio.NewReader(os.Stdin)
 	for {
 		resp, err := client.Chat.Completions.New(context.Background(),
 			openai.ChatCompletionNewParams{
-				Model: "qwen2.5:0.5b",
+				Model: model,
 				Messages:messages ,
 				Tools:tools,
 			},
@@ -85,24 +148,73 @@ func main() {
 		choice := resp.Choices[0]
 		msg := choice.Message
 		messages = append(messages, msg.ToParam())
-
+		// fmt.Fprintf(os.Stderr, "ToolCalls: %+v\n", msg.ToolCalls)
+		// fmt.Fprintf(os.Stderr, "Content: %s\n", msg.Content)
 		if len(msg.ToolCalls) > 0 {
 
 			for _, tc := range msg.ToolCalls{
 				switch tc.Function.Name {
 				case "Read":
 					var args ReadArgs
-    			if err := json.Unmarshal([]byte(tc.Function.Arguments), &args); err != nil {
-        		fmt.Fprintf(os.Stderr, "failed to parse Read args: %v\n", err)
-        		continue
-    			}
-					content, _ := os.ReadFile(args.FilePath)
+					if err := json.Unmarshal([]byte(tc.Function.Arguments), &args); err != nil {
+							messages = append(messages, openai.ToolMessage(fmt.Sprintf("error parsing args: %v", err), tc.ID))
+							continue
+					}
+					content, err := os.ReadFile(args.FilePath)
+					if err != nil {
+							messages = append(messages, openai.ToolMessage(fmt.Sprintf("error: %v", err), tc.ID))
+							continue
+					}
 					messages = append(messages, openai.ToolMessage(string(content), tc.ID))
+				case "Write":
+					var wargs model.WriteArgs
+					if err := json.Unmarshal([]byte(tc.Function.Arguments), &wargs); err != nil {
+							messages = append(messages, openai.ToolMessage(fmt.Sprintf("error parsing args: %v", err), tc.ID))
+							continue
+					}
+					err = os.WriteFile(wargs.FilePath, []byte(wargs.Content), 0644)
+					if err != nil {
+							messages = append(messages, openai.ToolMessage(fmt.Sprintf("error writing content: %v", err), tc.ID))
+							continue
+					} else {
+						messages = append(messages, openai.ToolMessage("File written successfully", tc.ID))
+					}
+				case "Execute":
+					var xargs model.ExecuteArgs 
+					if err := json.Unmarshal([]byte(tc.Function.Arguments), &xargs); err != nil {
+							messages = append(messages, openai.ToolMessage(fmt.Sprintf("error parsing args: %v", err), tc.ID))
+							continue
+					}
+					fmt.Printf("Execute: %q — confirm? [y/N]: ", xargs.Command)
+					input, err := reader.ReadString('\n')
+					input = strings.TrimSpace(input)
+					if input != "y" {
+						messages = append(messages, openai.ToolMessage(fmt.Sprintf("Command was rejected by admin"), tc.ID))
+						continue
+					}
+					var cmd *exec.Cmd 
+					if runtime.GOOS == "windows" {
+  			  	cmd = exec.Command("cmd", "/C", xargs.Command)
+					} else {
+    				cmd = exec.Command("sh", "-c", xargs.Command)
+					}
+					out, err := cmd.CombinedOutput()
+					fmt.Println(string(out))
+					if err != nil {
+							messages = append(messages, openai.ToolMessage(fmt.Sprintf("error executing the command: %v", err), tc.ID))
+							continue
+					} else {
+						messages = append(messages, openai.ToolMessage(string(out), tc.ID))
+					}
+
 				}
 			}
 		} else {
 			fmt.Print(msg.Content)
-			break
+			fmt.Println(" >>> ")
+			input, _ := reader.ReadString('\n')
+			input = strings.TrimSpace(input)
+			messages = append(messages, openai.UserMessage(string(input)))
 		}
 
 		// fmt.Print(resp.Choices[0].Message.Content)
